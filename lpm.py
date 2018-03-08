09d885023d81834945ba74748746f25d42475e6a
NoYzE1 <NoYzE@hotmail.de>
2018-03-08 21:00:28 +0100

    Update lpm.py

--- lpm.py
+++ lpm.py
@@ -7,12 +7,89 @@ def get_hexstring(length):
     for i in range(length):
         hexstring += random.choice(hexchars)
     return hexstring
+def save_entries():
+    with open("lpm.db", mode='w') as f:
+        f.write(ph+'\n')
+        for item in entries.items():
+            f.write(item[0]+'\n')
+            f.write(str(item[1]).lstrip('[').rstrip(']')+'\n')
+def encrypt(entry):
+    s = []
+    i = 0
+    for c in entry:
+        s.append(ord(c) ^ ord(p[i]))
+        i += 1
+    return s
+def decrypt(entry):
+    s = ""
+    i = 0
+    for c in entry:
+        s += chr(c ^ ord(p[i]))
+        i += 1
+    return s
+def new_entry():
+    print("0: Abort")
+    b = input("Entry Name: ")
+    if b == "0":
+        return
+    ps = ""
+    for i in range(32):
+        ps += random.choice(hexchars)
+    entries[b] = encrypt(ps)
+    save_entries()
+def create_entry(plain=False):
+    print("0: Abort")
+    b = input("Entry Name: ")
+    if b == "0":
+        return
+    if plain:
+        c = input("Password (Plain): ")
+    else:
+        c = getpass.getpass()
+    entries[b] = encrypt(c)
+    save_entries()
+def read_entry():
+    print_entries()
+    print("0: Abort")
+    b = int(input("Entry: "))
+    if b == 0:
+        return
+    print()
+    i = 1
+    for entry in entries:
+        if i == b:
+            print(decrypt(entries[entry]))
+            break
+        else:
+            i += 1
+def delete_entry():
+    print_entries()
+    print("0: Abort")
+    b = int(input("Entry: "))
+    if b == 0:
+        return
+    print()
+    i = 1
+    for entry in entries:
+        if i == b:
+            del entries[entry]
+            break
+        else:
+            i += 1
+def print_entries():
+    print()
+    i = 1
+    for entry in entries:
+        print("{0}: {1}".format(i, entry))
+        i += 1
+    print()
 first_run = False
 entries = {}
 try:
     with open("lpm.db", mode='r') as f:
         lines = f.readlines()
-        i = 0
+        ph = lines[0].strip('\n')
+        i = 1
         while i < len(lines)-1:
             l1 = lines[i].strip('\n')
             l2 = lines[i+1].strip('\n')
@@ -33,65 +110,20 @@ if first_run:
         p1 = hashlib.sha256(getpass.getpass().encode()).hexdigest()
         p2 = hashlib.sha256(getpass.getpass("Password again: ").encode()).hexdigest()
     p = p1
+    ph = hashlib.sha256(p.encode()).hexdigest()
+    save_entries()
 else:
-    p = hashlib.sha256(getpass.getpass().encode()).hexdigest()
-def save_entries():
-    with open("lpm.db", mode='w') as f:
-        for item in entries.items():
-            f.write(item[0]+'\n')
-            f.write(str(item[1]).lstrip('[').rstrip(']')+'\n')
-def encrypt(entry):
-    s = []
-    i = 0
-    for c in entry:
-        s.append(ord(c) ^ ord(p[i]))
-        i += 1
-    return s
-def decrypt(entry):
-    s = ""
-    i = 0
-    for c in entry:
-        s += chr(c ^ ord(p[i]))
-        i += 1
-    return s
-def new_entry():
-    b = input("Entry Name: ")
-    ps = ""
-    for i in range(32):
-        ps += random.choice(hexchars)
-    entries[b] = encrypt(ps)
-    save_entries()
-def create_entry():
-    b = input("Entry Name: ")
-    c = getpass.getpass()
-    entries[b] = encrypt(c)
-    save_entries()
-def read_entry():
-    print_entries()
-    b = int(input("Entry: "))
-    print()
-    i = 1
-    for entry in entries:
-        if i == b:
-            print(decrypt(entries[entry]))
-            break
-        else:
-            i += 1
-def delete_entry():
-    pass
-def print_entries():
-    print()
-    i = 1
-    for entry in entries:
-        print("{0}: {1}".format(i, entry))
-        i += 1
-    print()
+    p2 = None
+    while p2 != ph:
+        p = hashlib.sha256(getpass.getpass().encode()).hexdigest()
+        p2 = hashlib.sha256(p.encode()).hexdigest()
 while True:
     print()
     print("1: New Entry")
     print("2: Create Entry")
-    print("3: Read Entry")
-    print("4: Delete Entry")
+    print("3: Create Entry (Plain)")
+    print("4: Read Entry")
+    print("5: Delete Entry")
     print("0: Exit\n")
     a = input()
     if a == "0":
@@ -101,6 +133,8 @@ while True:
     elif a == "2":
         create_entry()
     elif a == "3":
-        read_entry()
+        create_entry(True)
     elif a == "4":
+        read_entry()
+    elif a == "5":
         delete_entry()
